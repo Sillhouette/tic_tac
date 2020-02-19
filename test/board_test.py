5ab5dc7128dce82c81e8a55527b40def3085e38f
github username <raustin.melchior@gmail.com>
2020-02-19 13:47:32 -0800

    add tests to check the stringified board with and without moves

--- test/board_test.py
+++ test/board_test.py
@@ -3,4 +3,35 @@ import unittest
 from src.board import Board
 
 class BoardTest(unittest.TestCase):
-    pass
+    def test_empty_board_to_s(self):
+       board = Board()
+       expected = [ 
+            #"⊱ –––––– {⋆⌘⋆} –––––– ⊰",
+            "",
+            "         |   |   ",
+            "      ---+---+---",
+            "         |   |   ",
+            "      ---+---+---",
+            "         |   |   ",
+            "",
+            #"⊱ –––––– {⋆⌘⋆} –––––– ⊰"
+       ]
+
+       self.assertEqual(expected, board.stringify_spaces())
+
+    def test_board_with_pieces(self):
+        board = Board()
+        board.spaces = ["X", " ", " ", "O", " ", "X", " ", " ", "O"]
+        expected = [
+            #"⊱ –––––– {⋆⌘⋆} –––––– ⊰",
+            "",
+            "       X |   |   ",
+            "      ---+---+---",
+            "       O |   | X ",
+            "      ---+---+---",
+            "         |   | O ",
+            "",
+            #"⊱ –––––– {⋆⌘⋆} –––––– ⊰"
+        ]
+
+        self.assertEqual(expected, board.stringify_spaces())
