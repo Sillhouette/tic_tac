38ba9d93f36c1ce62c61bb44c84eb8d4a5d5915a
github username <raustin.melchior@gmail.com>
2020-02-19 10:04:58 -0800

    add log, log_message, and log_messages to handle printing information to the console

--- src/cli.py
+++ src/cli.py
@@ -1,3 +1,17 @@
 class Cli():
+
+    def log(self, message, writer=print):
+        if isinstance(message, list):
+            self.log_messages(message, writer)
+        else:
+            self.log_message(message, writer)
+
+    def log_message(self, message, writer=print):
+        writer(message)
+
+    def log_messages(self, messages, writer=print):
+        for message in range(len(messages)):
+            writer(messages[message])
+    
     def welcome(self):
-        print("Hi! Welcome to Tic-Tac-Toe by Toenails Inc!")
+        print_message("Hi! Welcome to Tic-Tac-Toe by Toenails Inc!")
