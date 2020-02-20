9fff89fbbcb2fccf9deaa78c0d2475cc1481a588
github username <raustin.melchior@gmail.com>
2020-02-20 11:18:24 -0800

    add tests for update and turn_count

--- test/board_test.py
+++ test/board_test.py
@@ -49,3 +49,45 @@ class BoardTest(unittest.TestCase):
         board.spaces = ["X", "O", " ", "O", "X", " ", " ", "O", "X"]
 
         self.assertEqual(board.full(), False)
+
+    def test_update_board(self):
+        board = Board()
+        expected = [" ", "X", " ", " ", " ", " ", " ", " ", " "]
+
+        board.update(1, "X")
+        actual = board.spaces
+
+        self.assertEqual(expected, actual)
+
+
+    def test_update_board_2(self):
+        board = Board()
+        expected = [" ", " ", " ", " ", " ", "O", " ", " ", " "]
+
+        board.update(5, "O")
+        actual = board.spaces
+
+        self.assertEqual(expected, actual)
+
+    def test_turn_count(self):
+        board = Board()
+        expected = 1
+
+        board.update(1, "X")
+
+        actual = board.turn_count()
+
+        self.assertEqual(expected, actual)
+
+    def test_turn_count_2(self):
+        board = Board()
+        expected = 4
+
+        board.update(1, "X")
+        board.update(8, "O")
+        board.update(3, "X")
+        board.update(6, "O")
+
+        actual = board.turn_count()
+
+        self.assertEqual(expected, actual)
