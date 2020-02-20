b1e24702fa4642855d223bd31d8c9b79be05377d
github username <raustin.melchior@gmail.com>
2020-02-20 10:15:43 -0800

    add base test and game class

--- src/game.py
+++ src/game.py
@@ -1,2 +1,32 @@
+<<<<<<< Updated upstream
 class Game():
     pass
+=======
+from src.cli import Cli
+from src.board import Board
+from src.player import Player
+
+class Game():
+
+    def __init__(self, cli=Cli(), board=Board(), player_1=Player(),
+                 player_2=Player()):
+        self.cli = cli
+        self.board = board
+        self.player_1 = player_1
+        self.player_2 = player_2
+
+    def start(self):
+        self.cli.welcome()
+        self.play()
+
+    def play(self):
+        self.cli.log(self.board.stringify_spaces())
+        while not self.board.full():
+            self.turn()
+    
+    def turn(self):
+        move = self.cli.prompt_move()
+        self.board.update(current_player.token())
+        self.cli.log(self.board.stringify_spaces())
+        
+>>>>>>> Stashed changes
--- test/game_test.py
+++ test/game_test.py
@@ -3,5 +3,9 @@ import unittest
 from src.game import Game
 
 class GameTest(unittest.TestCase):
+<<<<<<< Updated upstream
     def test_dummy(self):
+=======
+    def test_im_not_sure_what_to_test_here(self):
+>>>>>>> Stashed changes
         pass
