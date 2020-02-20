5ad443c305f819527e9a084fc94583097d99c270
github username <raustin.melchior@gmail.com>
2020-02-20 11:08:23 -0800

    add tests for input_to_index and current_player

--- test/game_test.py
+++ test/game_test.py
@@ -2,10 +2,39 @@ import unittest
 
 from src.game import Game
 
+# I'm not sure how to unit test init, start, play, and turn
+# Should I just write integration tests for them?
+
 class GameTest(unittest.TestCase):
-<<<<<<< Updated upstream
-    def test_dummy(self):
-=======
-    def test_im_not_sure_what_to_test_here(self):
->>>>>>> Stashed changes
-        pass
+    def test_input_to_index(self):
+        game = Game()
+        expected = 0
+
+        actual = game.input_to_index("1")
+
+        self.assertEqual(expected, actual)
+
+    def test_input_to_index_2(self):
+        game = Game()
+        expected = 5
+
+        actual = game.input_to_index("6")
+
+        self.assertEqual(expected, actual)
+
+    def test_current_player(self):
+        game = Game()
+        expected = game.player_1
+
+        actual = game.current_player()
+
+        self.assertEqual(expected, actual)
+
+    def test_current_player_2(self):
+        game = Game()
+        game.board.spaces[0] = "X"
+        expected = game.player_2
+
+        actual = game.current_player()
+
+        self.assertEqual(expected, actual)
