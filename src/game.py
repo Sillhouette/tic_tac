5f72abfa8779367fa10bff7d2e3b0c282512743e
github username <raustin.melchior@gmail.com>
2020-02-20 10:54:44 -0800

    add game class, constructor to build a game, start, play, turn, input_to_index, and current_player methods

--- src/game.py
+++ src/game.py
@@ -1,15 +1,11 @@
-<<<<<<< Updated upstream
-class Game():
-    pass
-=======
 from src.cli import Cli
 from src.board import Board
 from src.player import Player
 
 class Game():
 
-    def __init__(self, cli=Cli(), board=Board(), player_1=Player(),
-                 player_2=Player()):
+    def __init__(self, cli=Cli(), board=Board(), player_1=Player(token="X"),
+                 player_2=Player(token="O")):
         self.cli = cli
         self.board = board
         self.player_1 = player_1
@@ -23,10 +19,22 @@ class Game():
         self.cli.log(self.board.stringify_spaces())
         while not self.board.full():
             self.turn()
+
+        self.cli.goodbye()
     
     def turn(self):
-        move = self.cli.prompt_move()
-        self.board.update(current_player.token())
+        player_choice = self.cli.prompt_user()
+        move = self.input_to_index(int(player_choice))
+
+        self.board.update(move, self.current_player().token)
         self.cli.log(self.board.stringify_spaces())
-        
->>>>>>> Stashed changes
+       
+    def input_to_index(self, user_input):
+        return user_input - 1 
+
+    def current_player(self):
+        if self.board.turn_count() % 2 == 0:
+            return self.player_1
+        else:
+            return self.player_2
+
