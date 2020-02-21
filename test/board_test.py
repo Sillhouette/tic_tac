3f88507e19002a77e73c4d744d9faed99bbc9691
github username <raustin.melchior@gmail.com>
2020-02-21 10:59:43 -0800

    add tests for valid_move, position_taken, and within_bounds

--- test/board_test.py
+++ test/board_test.py
@@ -59,3 +59,69 @@ class BoardTest(unittest.TestCase):
         actual = board.turn_count()
 
         self.assertEqual(expected, actual)
+    
+    def test_valid_move(self):
+        board = Board()
+        position = 4
+        expected = True
+
+        actual = board.valid_move(position)
+
+        self.assertEqual(expected, actual)
+
+    def test_invalid_move(self):
+        board = Board()
+        position = 15
+        expected = False
+
+        actual = board.valid_move(position)
+
+        self.assertEqual(expected, actual)
+
+    def test_invalid_move_2(self):
+        board = Board()
+        position = 6
+        board.spaces[position] = "X"
+        expected = False
+
+        actual = board.valid_move(position)
+
+        self.assertEqual(expected, actual)
+
+    def test_position_taken(self):
+        board = Board()
+        position = 4
+        board.spaces[position] = "X"
+        expected = True
+
+        actual = board.position_taken(position)
+
+        self.assertEqual(expected, actual)
+   
+    def test_position_not_taken(self):
+        board = Board()
+        position = 4
+        expected = False
+
+        actual = board.position_taken(position)
+
+        self.assertEqual(expected, actual)
+
+    def test_within_board(self):
+        board = Board()
+        position = 5
+        expected = True
+
+        actual = board.within_board(position)
+
+        self.assertEqual(expected, actual)
+
+    def test_position_out_of_bounds(self):
+        board = Board()
+        position = 50
+        expected = False
+
+        actual = board.within_board(position)
+
+        self.assertEqual(expected, actual)
+
