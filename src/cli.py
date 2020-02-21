3b7b8caeccdff07d88b6d749bb4caed9d0ffa12e
github username <raustin.melchior@gmail.com>
2020-02-21 08:40:29 -0800

    adjust the way the board displays

--- src/cli.py
+++ src/cli.py
@@ -1,6 +1,4 @@
 class Cli():
-    board_border = "⊱ –––––– {⋆⌘⋆} –––––– ⊰",
-    board_divider = "      ---+---+---"
 
     def __init__(self, writer=print, reader=input):
         self.writer = writer
@@ -29,14 +27,18 @@ class Cli():
         self.log("You played a great game! See you next time!")
 
     def display_board(self, board):
-        board = f"""\
+        border = "⊱ –––––– {⋆⌘⋆} –––––– ⊰"
+        divider = "      ---+---+---"
+        board = f"""
+{border}
 
        {board.spaces[0]} | {board.spaces[1]} | {board.spaces[2]} 
-{Cli.board_divider}
+{divider}
        {board.spaces[3]} | {board.spaces[4]} | {board.spaces[5]} 
-{Cli.board_divider}
+{divider}
        {board.spaces[6]} | {board.spaces[7]} | {board.spaces[8]} 
-\
+
+{border}
 """
         self.log(board)
         return board
