9dd3d97c3ad4f287540a6d94733a2856403bd45e
github username <raustin.melchior@gmail.com>
2020-02-21 08:14:54 -0800

    removed the stringify method from display board as well as the other cli-related class variables

--- src/board.py
+++ src/board.py
@@ -1,25 +1,11 @@
 class Board:
     SIZE = 9
     border = "⊱ –––––– {⋆⌘⋆} –––––– ⊰",
-    divider = "      ---+---+---"
     space = " "
 
     def __init__(self):
         self.spaces = [Board.space] * 9
 
-    def stringify_spaces(self):
-        return [
-            #Board.border,
-            "",
-            f"       {self.spaces[0]} | {self.spaces[1]} | {self.spaces[2]} ",
-            Board.divider,
-            f"       {self.spaces[3]} | {self.spaces[4]} | {self.spaces[5]} ", 
-            Board.divider,
-            f"       {self.spaces[6]} | {self.spaces[7]} | {self.spaces[8]} ",
-            "",
-            #Board.border 
-        ]
-    
     def full(self):
         return not Board.space in self.spaces
 
