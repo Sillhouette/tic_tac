04b2804f425897999bd0c5f614826a48b543efbc
github username <raustin.melchior@gmail.com>
2020-02-28 13:10:59 -0800

    create helper methods to make the code intent clearer

--- src/game.py
+++ src/game.py
@@ -9,6 +9,7 @@ class Game:
         self.cli = cli
         self.players = players
         self.board = board
+        self.validator = Validator()
         self.game_in_process = True
         self.possible_results = self.cli.build_possible_results(self.players)
         self.actions = {
@@ -19,18 +20,23 @@ class Game:
 
     def play(self):
         result = None
-        validator = Validator()
         self.cli.print_board(self.board)
         while(self.game_in_process):
             current_player = self.current_player()
             player_choice = self.cli.request_move(current_player)
-            selected_action, move = validator.validate_move(player_choice, self.board)
+            selected_action, move = self.validator.validate_move(player_choice, self.board)
             result = self.actions[selected_action](move, current_player.token)
-            if result != constants.EXIT: self.cli.print_board(self.board)
-            if result in self.possible_results: self.game_in_process = False
+            if not self.player_chose_exit(result): self.cli.print_board(self.board)
+            if self.game_over(result): self.game_in_process = False
 
         self.cli.log(self.possible_results[result])
-   
+    
+    def player_chose_exit(self, result):
+        return result == constants.EXIT
+
+    def game_over(self, result):
+        return result in self.possible_results
+        
     def current_player(self):
         num_players = len(self.players)
         turns_taken = self.board.turn_count()
