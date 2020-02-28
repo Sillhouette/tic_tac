be11fa2d89525315dc2024f83535619e70abcd1d
github username <raustin.melchior@gmail.com>
2020-02-28 12:41:20 -0800

    move build_results to cli and setup better constant usage

--- src/cli.py
+++ src/cli.py
@@ -1,18 +1,13 @@
+import src.constants as constants
+
 class Cli():
-    EXIT = "exit"
-    WELCOME = "welcome"
-    FINISHED = "finished"
-    ERROR = "error"
-    WIN = "win"
-    MOVE = "move"
-    REQUEST_MOVE = "request"
     MESSAGES = {
-        WELCOME: "Hi! Welcome to Tic-Tac by Toenails Inc!",
-        EXIT: "Leaving so soon? Hope to see you back again shortly!",
-        FINISHED: "You played a great game! See you next time!",
-        ERROR: "\nI'm sorry, it seems you may have accidently made an invalid move, can you please try another position?",
-        WIN: (lambda token: f"{token} wins!"),
-        REQUEST_MOVE: (lambda token: f"It's {token}'s turn! Please select a square using 1-9:\n")
+        constants.WELCOME: "Hi! Welcome to Tic-Tac by Toenails Inc!",
+        constants.EXIT: "Leaving so soon? Hope to see you back again shortly!",
+        constants.FINISHED: "You played a great game! See you next time!",
+        constants.ERROR: "\nI'm sorry, it seems you may have accidently made an invalid move, can you please try another position?",
+        constants.WIN: (lambda token: f"{token} wins!"),
+        constants.REQUEST_MOVE: (lambda token: f"It's {token}'s turn! Please select a square using 1-9:\n")
     }
 
     def __init__(self, writer=print, reader=input):
@@ -26,22 +21,33 @@ class Cli():
         return self.reader(message).lower()
 
     def welcome(self):
-        self.log(self.MESSAGES[self.WELCOME])
+        self.log(self.MESSAGES[constants.WELCOME])
         
     def handle_game_end(self):
-        self.log(self.MESSAGES[self.FINISHED])
+        self.log(self.MESSAGES[constants.FINISHED])
         
     def handle_exit(self):
-        self.log(self.MESSAGES[self.EXIT])
+        self.log(self.MESSAGES[constants.EXIT])
 
     def get_player_tokens(self):
         return ["X", "O"]
 
     def get_board_type(self):
         return "3x3"
+    
+    def build_possible_results(self, players):
+        results = { 
+            constants.EXIT: self.MESSAGES[constants.EXIT],
+            constants.FINISHED: self.MESSAGES[constants.FINISHED]
+        }
 
+        for player in players:
+            results[player.token] = self.MESSAGES[constants.WIN](player.token) 
+
+        return results
+    
     def request_move(self, player):
-         turn_prompt = self.MESSAGES[self.REQUEST_MOVE](player.token) 
+         turn_prompt = self.MESSAGES[constants.REQUEST_MOVE](player.token) 
          return self.prompt_user(turn_prompt)
 
     def print_board(self, board):
@@ -68,5 +74,5 @@ class Cli():
         self.log(board_string)
 
     def invalid_move(self):
-        self.log(self.MESSAGES[self.ERROR])
+        self.log(self.MESSAGES[constants.ERROR])
 
