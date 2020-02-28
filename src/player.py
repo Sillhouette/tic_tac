2c947d75b5fe05341102240b3199ee2012330c57
github username <raustin.melchior@gmail.com>
2020-02-28 07:36:07 -0800

    no change

--- src/player.py
+++ src/player.py
@@ -2,6 +2,6 @@ class Player():
     def __init__(self, name="player", token="X"):
         self.name = name
         self.token = token
-
+    
     def set_token(self, token):
        self.token = token
