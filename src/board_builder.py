a31f67fb5d09f9be30ae11a50275b114112a5e37
github username <raustin.melchior@gmail.com>
2020-02-28 07:32:55 -0800

    build BoardBuilder class that can generate boards based on an input

---
+++ src/board_builder.py
@@ -0,0 +1,8 @@
+from src.three_by_three_board import ThreeByThreeBoard
+
+class BoardBuilder():
+    THREE_BY_THREE = "3x3"
+
+    def build_board(self, board_type):
+        board = ThreeByThreeBoard()
+        return board
