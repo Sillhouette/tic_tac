f659aeed1ddb91d797d3a4f54b89ef00eb34c398
github username <raustin.melchior@gmail.com>
2020-02-28 07:41:51 -0800

    add tests for the PlayerBuilder class

---
+++ test/player_builder_test.py
@@ -0,0 +1,22 @@
+import unittest
+
+from src.player_builder import PlayerBuilder
+from src.player import Player
+
+class PlayerBuilderTest(unittest.TestCase):
+
+    def test_build_players(self):
+        builder = PlayerBuilder()
+        tokens = ['X', 'O']
+        expected = True
+        expected_size = 2
+
+        players = builder.build_players(tokens)
+        actual = all(isinstance(player, Player) for player in players)
+        actual_size = len(players)
+
+        self.assertEqual(expected, actual)
+        self.assertEqual(expected_size, actual_size)
+
+
+
