0ea8be1a6e937ff1ccf668afbd3e409116ac7637
github username <raustin.melchior@gmail.com>
2020-02-28 19:24:51 -0800

    implement global constants

--- test/board_builder_test.py
+++ test/board_builder_test.py
@@ -1,11 +1,12 @@
 import unittest
+import src.constants as constants
 
 from src.board_builder import BoardBuilder
 
 class BoardBuilderTest(unittest.TestCase):
     def test_setup_3x3_board(self):
         builder = BoardBuilder()
-        board_type = BoardBuilder.THREE_BY_THREE
+        board_type = constants.THREE_BY_THREE
         expected = board_type 
 
         board = builder.build_board(board_type)
