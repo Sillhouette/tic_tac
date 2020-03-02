986970ae3d23ed121e6d45df78c28f4a1025ba98
github username <raustin.melchior@gmail.com>
2020-03-02 10:16:36 -0800

    add tests to test functionality of the initialize method

--- test/app_test.py
+++ test/app_test.py
@@ -1,11 +1,67 @@
 import unittest
 
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
+from src.game import Game
 from src.app import App
 from src.player import Player
 from src.three_by_three_board import ThreeByThreeBoard
+from src.three_by_three_validator import ThreeByThreeValidator
 
 class AppTest(unittest.TestCase):
+    @patch.object(Game, "play")
+    def test_initialize_welcomes_player(self, game_play):
+        cli = Mock()
+        app = App(cli)
+        app.setup_players = Mock()
+        
+        expected = app.initialize()
+
+        cli.welcome.assert_called()
+   
+    @patch.object(Game, "play")
+    def test_initialize_sets_players(self, game_play):
+        cli = Mock()
+        app = App(cli)
+        app.setup_players = Mock()
+        
+        expected = app.initialize()
+
+        app.setup_players.assert_called()
+   
+    @patch.object(Game, "play")
+    def test_initialize_sets_board(self, game_play):
+        cli = Mock()
+        app = App(cli)
+        app.setup_players = Mock()
+        app.setup_board = Mock()
+
+        expected = app.initialize()
+
+        app.setup_board.assert_called()
+    
+    @patch.object(Game, "play")
+    def test_initialize_sets_validator(self, game_play):
+        cli = Mock()
+        app = App(cli)
+        app.setup_players = Mock()
+        app.setup_validator = Mock()
+
+        expected = app.initialize()
+
+        app.setup_validator.assert_called()
+    
+    @patch.object(Game, "__init__", lambda a, b, c, d, e: None)
+    @patch.object(Game, "play")
+    def test_initialize_starts_game(self, game_play):
+        cli = Mock()
+        app = App(cli)
+        app.setup_players = Mock()
+
+        expected = app.initialize()
+
+        app.game.play.assert_called()
+
+
     def test_setup_players(self):
         cli = Mock()
         cli.get_player_tokens = Mock()
@@ -30,5 +86,17 @@ class AppTest(unittest.TestCase):
 
         board = app.setup_board()
         actual = isinstance(board, ThreeByThreeBoard)
+
+        self.assertEqual(expected, actual)
+
+    def test_setup_validator(self):
+        cli = Mock()
+        board = Mock()
+        board.type = "3x3"
+        app = App(cli)
+        expected = True
+
+        validator = app.setup_validator(board)
+        actual = isinstance(validator, ThreeByThreeValidator)
 
         self.assertEqual(expected, actual)
