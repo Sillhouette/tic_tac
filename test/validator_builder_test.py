88cbde8b33fdb22af1c7e40bb6bd2ffc1ae91135
github username <raustin.melchior@gmail.com>
2020-03-04 08:05:58 -0800

    change tests to reflect changes in how the validator_builder works

--- test/validator_builder_test.py
+++ test/validator_builder_test.py
@@ -2,14 +2,14 @@ import unittest
 import src.constants as constants
 
 from src.validator_builder import ValidatorBuilder
+from src.three_by_three_board import ThreeByThreeBoard
 
 class ValidatorBuilderTest(unittest.TestCase):
-    def test_build_validator_3x3(self):
+    def test_build_validator_can_build_3x3_board(self):
         builder = ValidatorBuilder()
-        validator_type = constants.THREE_BY_THREE
-        expected = validator_type
+        expected = ThreeByThreeBoard() 
 
-        validator = builder.build_validator(validator_type)
-        actual = validator.type
+        validator = builder.build_validator(expected)
+        actual = validator.board
 
         self.assertEqual(expected, actual)
