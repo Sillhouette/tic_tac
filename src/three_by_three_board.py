6aa97a326ff7541cd86f2f5e6fb3d9fe5451b2e3
github username <raustin.melchior@gmail.com>
2020-03-04 07:38:57 -0800

    move valid move, position_taken, and within_board to board, add winner method, add winning combos, implement winner method in game_result

--- src/three_by_three_board.py
+++ src/three_by_three_board.py
@@ -1,7 +1,20 @@
+import src.constants as constants
+
 class ThreeByThreeBoard():
+    # => All of the possible winning combinations
+    WIN_COMBINATIONS = [
+        [0, 1, 2], # Top row
+        [3, 4, 5], # Middle row
+        [6, 7, 8], # Bottom row
+        [0, 3, 6], # First column
+        [1, 4, 7], # Second column
+        [2, 5, 8], # Third Column
+        [0, 4, 8], # Left to Right diagonal
+        [2, 4, 6]  # Right to Left diagonal
+    ]
 
     def __init__(self):
-        self.type = "3x3"
+        self.type = constants.THREE_BY_THREE 
         self.size = 9
         self.spaces = [None] * self.size
 
@@ -10,7 +23,9 @@ class ThreeByThreeBoard():
     
     def move_result(self, move, token):
         self.update(move, token)
-        if self.full(): return "finished"
+        winner = self.winner()
+        if winner: return winner
+        elif self.full(): return constants.CATS 
 
     def full(self):
         return not None in self.spaces
@@ -21,3 +36,24 @@ class ThreeByThreeBoard():
 
     def move_to_index(self, move):
         return int(move) - 1
+
+    def valid_move(self, move):
+        position = self.move_to_index(move)
+        return self.within_board(position) and not self.position_taken(position)
+
+    def position_taken(self, position):
+        return self.spaces[position] != None
+
+    def within_board(self, position):
+        return position >= 0 and position <= self.size
+
+    def winner(self):
+        winner = None
+        for combo in self.WIN_COMBINATIONS:
+            win_index_1, win_index_2, win_index_3 = combo
+            if self.spaces[win_index_1] == self.spaces[win_index_2] and\
+                self.spaces[win_index_2] == self.spaces[win_index_3] and\
+                self.position_taken(win_index_1):
+                winner = self.spaces[win_index_1]
+
+        return winner
