5bf6d6e01d91325db4291533c2b4420985f3e434
github username <raustin.melchior@gmail.com>
2020-03-04 07:41:15 -0800

    change naming to be more concise

--- test/board_builder_test.py
+++ test/board_builder_test.py
@@ -4,7 +4,7 @@ import src.constants as constants
 from src.board_builder import BoardBuilder
 
 class BoardBuilderTest(unittest.TestCase):
-    def test_setup_3x3_board(self):
+    def test_setup_board_can_setup_3x3_board(self):
         builder = BoardBuilder()
         board_type = constants.THREE_BY_THREE
         expected = board_type 
