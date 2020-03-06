3afadb2df0898ba7004bd19cefd77da530458201
github username <raustin.melchior@gmail.com>
2020-03-06 08:35:03 -0800

    add unit tests for the refactored methods and colorization of the empty board spaces

--- test/three_by_three_presenter_test.py
+++ test/three_by_three_presenter_test.py
@@ -5,7 +5,47 @@ from src.three_by_three_presenter import ThreeByThreePresenter
 from src.three_by_three_board import ThreeByThreeBoard
 
 class ThreeByThreePresenterTest(unittest.TestCase):
+
+    def test_colorize_empty_space_index_properly_returns(self):
+        presenter = ThreeByThreePresenter()
+        index = 1
+        expected = f"{constants.EMPTY_INDEX_COLOR_START}{index}{constants.EMPTY_INDEX_COLOR_END}"
+
+        actual = presenter.colorize_empty_space_index(index)
+
+        self.assertEqual(expected, actual)
+
+    def test_scrub_board_returns_properly_scrubbed_empty_board(self):
+        presenter = ThreeByThreePresenter()
+        board = ThreeByThreeBoard()
+        expected = []
+        for index in list(range(1, 10)):
+            expected.append(presenter.colorize_empty_space_index(index))
     
+        actual = presenter.scrub_board(board)
+
+        self.assertEqual(expected, actual)
+
+    def test_scrub_board_returns_properly_scrubbed_in_progress_board(self):
+        presenter = ThreeByThreePresenter()
+        board = ThreeByThreeBoard()
+        moves = ["1", "4", "7", "3"]
+        tokens = ["X", "O", "X", "O"]
+        for i, move in enumerate(moves):
+            board.update(move, tokens[i])
+
+        expected = []
+        for index, token in enumerate(board.spaces):
+            if token == None:
+                expected.append(presenter.colorize_empty_space_index(index + 1))
+            else:
+                expected.append(token)
+
+        actual = presenter.scrub_board(board)
+
+        self.assertEqual(expected, actual)
+
+
     def test_present_empty_board(self):
         board = ThreeByThreeBoard()
         presenter = ThreeByThreePresenter()
