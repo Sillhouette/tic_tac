8239353bb9b113908b38780cbe27318a0c41e439
github username <raustin.melchior@gmail.com>
2020-03-06 08:47:43 -0800

    give computer player cli access so he can communicate information to the player

--- src/computer_player.py
+++ src/computer_player.py
@@ -2,14 +2,17 @@ import random
 import src.constants as constants
 
 class ComputerPlayer():
-    def __init__(self, board, token="O"):
+    def __init__(self, board, cli, token="O"):
         self.token = token
         self.board = board
+        self.cli = cli
         
     def set_token(self, token):
         self.token = token
 
     def get_move(self):
+        self.cli.notify_for_computer_turn()
+
         valid_space = False
         
         while valid_space == False:
