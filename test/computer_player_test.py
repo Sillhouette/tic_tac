0485573f72dc413060b3f48e2233ff15546c33c8
github username <raustin.melchior@gmail.com>
2020-03-06 06:49:34 -0800

    add tests to test the computer players move and set_token functions

--- test/computer_player_test.py
+++ test/computer_player_test.py
@@ -1,6 +1,31 @@
 import unittest
 
+from unittest.mock import Mock
 from src.computer_player import ComputerPlayer
+from src.three_by_three_board import ThreeByThreeBoard
 
 class ComputerPlayerTest(unittest.TestCase):
-    pass
+    def test_setting_computer_player_token(self):
+        board = Mock()
+        computer = ComputerPlayer(board)
+        token = "%"
+        expected = token
+
+        computer.set_token(token)
+        actual = computer.token
+
+        self.assertEqual(expected, actual)
+
+    def test_get_move_returns_a_valid_move_every_turn(self):
+        board = ThreeByThreeBoard()
+        computer = ComputerPlayer(board)
+        expected = True
+
+        for space in board.spaces:
+            move = computer.get_move()[1]
+            index = board.move_to_index(move)
+            if not board.position_taken(index):
+                board.update(move, computer.token)
+        actual = board.full()
+
+        self.assertEqual(expected, actual)
