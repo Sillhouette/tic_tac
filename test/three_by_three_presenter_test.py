965283909a3a0ca5f6310adbee5faa4d70afc7d6
github username <raustin.melchior@gmail.com>
2020-03-06 07:53:57 -0800

    adjust present_board tests to account for the colorized printed indicies on the board

--- test/three_by_three_presenter_test.py
+++ test/three_by_three_presenter_test.py
@@ -1,4 +1,5 @@
 import unittest
+import src.constants as constants
 
 from src.three_by_three_presenter import ThreeByThreePresenter
 from src.three_by_three_board import ThreeByThreeBoard
@@ -8,17 +9,18 @@ class ThreeByThreePresenterTest(unittest.TestCase):
     def test_present_empty_board(self):
         board = ThreeByThreeBoard()
         presenter = ThreeByThreePresenter()
+        colorize = (lambda i: f"{constants.EMPTY_INDEX_COLOR_START}{i}{constants.EMPTY_INDEX_COLOR_END}")
 
-        expected =  """
-⊱ –––––– {⋆⌘⋆} –––––– ⊰
+        expected =  f"""
+{presenter.border}
 
-         |   |   
+       {colorize(1)} | {colorize(2)} | {colorize(3)} 
       ---+---+---
-         |   |   
+       {colorize(4)} | {colorize(5)} | {colorize(6)} 
       ---+---+---
-         |   |   
+       {colorize(7)} | {colorize(8)} | {colorize(9)} 
 
-⊱ –––––– {⋆⌘⋆} –––––– ⊰
+{presenter.border}
 """
         actual = presenter.present_board(board)
 
@@ -26,19 +28,20 @@ class ThreeByThreePresenterTest(unittest.TestCase):
 
     def test_present_board_in_progress(self):
         board = ThreeByThreeBoard()
-        board.spaces = ["X", " ", " ", "O", " ", "X", " ", " ", "O"]
         presenter = ThreeByThreePresenter()
-       
-        expected = """
-⊱ –––––– {⋆⌘⋆} –––––– ⊰
+        board.spaces = ["X", None, None, "O", None, "X", None, None, "O"]
+        colorize = (lambda i: f"{constants.EMPTY_INDEX_COLOR_START}{i}{constants.EMPTY_INDEX_COLOR_END}")
 
-       X |   |   
-      ---+---+---
-       O |   | X 
-      ---+---+---
-         |   | O 
+        expected =  f"""
+{presenter.border}
 
-⊱ –––––– {⋆⌘⋆} –––––– ⊰
+       X | {colorize(2)} | {colorize(3)} 
+      ---+---+---
+       O | {colorize(5)} | X 
+      ---+---+---
+       {colorize(7)} | {colorize(8)} | O 
+
+{presenter.border}
 """
         actual = presenter.present_board(board)
         
