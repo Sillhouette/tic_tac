f45f038bfefc99a74ab9508db54c254086998bd5
github username <raustin.melchior@gmail.com>
2020-03-09 14:05:14 -0700

    implement processor in validator builder

--- src/validator_builder.py
+++ src/validator_builder.py
@@ -3,5 +3,5 @@ import src.constants as constants
 from src.three_by_three_validator import ThreeByThreeValidator
 
 class ValidatorBuilder():
-    def build_validator(self, board):
-       return ThreeByThreeValidator(board)
+    def build_validator(self, processor):
+       return ThreeByThreeValidator(processor)
