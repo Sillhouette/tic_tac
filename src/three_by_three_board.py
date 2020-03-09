cf7acfd5836b1bcd7d611e9f85a490f55306908a
github username <raustin.melchior@gmail.com>
2020-03-09 12:02:06 -0700

    refactor for readability` `

--- src/three_by_three_board.py
+++ src/three_by_three_board.py
@@ -2,20 +2,20 @@ import src.constants as constants
 
 class ThreeByThreeBoard():
     # => All of the possible winning combinations
-    WIN_COMBINATIONS = [
-        [0, 1, 2], # Top row
-        [3, 4, 5], # Middle row
-        [6, 7, 8], # Bottom row
-        [0, 3, 6], # First column
-        [1, 4, 7], # Second column
-        [2, 5, 8], # Third Column
-        [0, 4, 8], # Left to Right diagonal
-        [2, 4, 6]  # Right to Left diagonal
-    ]
+    WIN_COMBINATIONS = {
+        "Top Row": [0, 1, 2],
+        "Middle Row": [3, 4, 5],
+        "Bottom Row": [6, 7, 8],
+        "First Column": [0, 3, 6],
+        "Second Column": [1, 4, 7],
+        "Third Column": [2, 5, 8],
+        "Left to Right Diagonal": [0, 4, 8],
+        "Right to Left Diagonal": [2, 4, 6]
+    }
 
     def __init__(self):
         self.type = constants.THREE_BY_THREE 
-        self.size = 9
+        self.size = 3 * 3
         self.spaces = [None] * self.size
 
     def turn_count(self):
@@ -58,12 +58,15 @@ class ThreeByThreeBoard():
         
         return [constants.ERROR, move]
 
+    def compare_board_indicies(self, index_1, index_2):
+        return self.spaces[index_1] == self.spaces[index_2]
+
     def winner(self):
         winner = None
-        for combo in self.WIN_COMBINATIONS:
+        for combo in self.WIN_COMBINATIONS.values():
             win_index_1, win_index_2, win_index_3 = combo
-            if self.spaces[win_index_1] == self.spaces[win_index_2] and\
-                self.spaces[win_index_2] == self.spaces[win_index_3] and\
+            if self.compare_board_indicies(win_index_1, win_index_2) and\
+                self.compare_board_indicies(win_index_2, win_index_3) and\
                 self.position_taken(win_index_1):
                 winner = self.spaces[win_index_1]
 
