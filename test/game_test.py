138a997c8e45ff608b239bb130d2d4dee69e7177
Austin Melchior <raustin.melchior@gmail.com>
2020-03-16 11:02:28 -0700

    add more tests to better test game loop integration test

--- test/game_test.py
+++ test/game_test.py
@@ -9,7 +9,7 @@ from src.three_by_three_board import ThreeByThreeBoard
 from src.three_by_three_processor import ThreeByThreeProcessor
 
 class GameTest(unittest.TestCase):
-    def test_game_loop_plays_full_game(self):
+    def create_integrated_game(self):
         reader, writer = Mock(), Mock()
         cli = Cli(writer, reader)
         cli.print_board = Mock()
@@ -17,6 +17,18 @@ class GameTest(unittest.TestCase):
         player_1, player_2 = Mock(HumanPlayer), Mock(HumanPlayer)
         player_1.token = "X"
         player_2.token = "O"
+        players = [player_1, player_2]
+        board = ThreeByThreeBoard()
+        processor = ThreeByThreeProcessor(board)
+        processor.set_players(players)
+        game = Game(cli, players, board, processor)
+        return game
+
+    def test_game_loop_plays_full_game(self):
+        game = self.create_integrated_game()
+        cli = game.cli
+        player_1 = game.players[0]
+        player_2 = game.players[1]
         action = constants.MOVE
         player_1.get_move.side_effect = [
             [action, "1"],
@@ -31,11 +43,6 @@ class GameTest(unittest.TestCase):
             [action, "7"],
             [action, "9"]
         ]
-        players = [player_1, player_2]
-        board = ThreeByThreeBoard()
-        processor = ThreeByThreeProcessor(board)
-        processor.set_players(players)
-        game = Game(cli, players, board, processor)
         expected = True
         expected_print_board_call_count = 10
         expected_p1_get_move_count = 5
@@ -50,7 +57,69 @@ class GameTest(unittest.TestCase):
         self.assertEqual(expected_print_board_call_count, print_board_call_count)
         self.assertEqual(expected_p1_get_move_count, p1_get_move_count)
         self.assertEqual(expected_p2_get_move_count, p2_get_move_count)
-        cli.log.assert_called_with("Cats game! You all played phenomenally!")
+        cli.log.assert_called_with(cli.MESSAGES[constants.CATS])
+
+    def test_game_loop_stops_when_player_wins(self):
+        game = self.create_integrated_game()
+        cli = game.cli
+        player_1 = game.players[0]
+        player_2 = game.players[1]
+        action = constants.MOVE
+        player_1.get_move.side_effect = [
+            [action, "1"],
+            [action, "3"],
+            [action, "2"],
+        ]
+        player_2.get_move.side_effect = [
+            [action, "4"],
+            [action, "6"],
+        ]
+        expected = True
+        expected_print_board_call_count = 6
+        expected_p1_get_move_count = 3
+        expected_p2_get_move_count = 2
+
+        game.play()
+
+        print_board_call_count = cli.print_board.call_count
+        p1_get_move_count = player_1.get_move.call_count
+        p2_get_move_count = player_2.get_move.call_count
+
+        self.assertEqual(expected_print_board_call_count, print_board_call_count)
+        self.assertEqual(expected_p1_get_move_count, p1_get_move_count)
+        self.assertEqual(expected_p2_get_move_count, p2_get_move_count)
+        cli.log.assert_called_with(cli.MESSAGES[constants.WIN](player_1.token))
+
+    def test_game_loop_stops_when_player_chooses_to_exit(self):
+        game = self.create_integrated_game()
+        cli = game.cli
+        player_1 = game.players[0]
+        player_2 = game.players[1]
+        action = constants.MOVE
+        player_1.get_move.side_effect = [
+            [action, "1"],
+            [action, "3"],
+            [constants.EXIT, None],
+        ]
+        player_2.get_move.side_effect = [
+            [action, "4"],
+            [action, "6"],
+        ]
+        expected = True
+        expected_print_board_call_count = 5
+        expected_p1_get_move_count = 3
+        expected_p2_get_move_count = 2
+
+        game.play()
+
+        print_board_call_count = cli.print_board.call_count
+        p1_get_move_count = player_1.get_move.call_count
+        p2_get_move_count = player_2.get_move.call_count
+
+        self.assertEqual(expected_print_board_call_count, print_board_call_count)
+        self.assertEqual(expected_p1_get_move_count, p1_get_move_count)
+        self.assertEqual(expected_p2_get_move_count, p2_get_move_count)
+        cli.log.assert_called_with(cli.MESSAGES[constants.EXIT])
 
     def test_player_chose_exit_returns_false_when_given_exit(self):
         cli = Mock()
